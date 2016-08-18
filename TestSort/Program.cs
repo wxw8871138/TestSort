fbcd1e390cf51970a139483491618f94f6c6151e
WangXinwei <315439889@qq.com>
2016-08-18 16:13:19 +0800

    sort

---
+++ TestSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 32, 34, 29, 0, 4, 9 };
+            for (int i = 0; i < a.Length-1; i++)
+            {
+                for(int j = i+1; j < a.Length; j++)
+                {
+                    if (a[i] < a[j])
+                    {
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+            for(int k = 0; k< a.Length; k++)
+            {
+                Console.Write(a[k] + " ");
+            }
+        }
+    }
+}
